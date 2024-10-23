59bc64a7f905658401c0686053905ccb7e0757b6
Ryan Gleeson <71162701+PracticalRyan@users.noreply.github.com>
2024-10-23 17:50:51 +0700

    Added functions for separation

--- main.c
+++ main.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
+// Functions to set variables
+
+void setAir()
+{
+}
+
+void setEnvironment()
+{
+}
+
+// Functions to run simulation
+
+void runSimulation()
+{
+}
 
 void main()
 {
-    printf("Hello World");
+    printf("What would you like to do?");
+    printf("[C] Control Airconditioner [E] Set Environment [R] Run Simulation & View Dashboard");
 }
